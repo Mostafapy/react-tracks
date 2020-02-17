72869f1e3ddd3771006726ba40e35609446abf9d
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-02-17 08:26:03 -0800

    adding Mutation and createtrack class to the schemas

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -15,4 +15,20 @@ class Query(graphene.ObjectType):
     def resolve_tracks(self, info):
         """ Returns list of track objects"""
         return Track.objects.all()
-    
+
+class CreateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+        
+
+    def mutate(self, info, title, description, url):
+        track = Track(title=title, description=description, url=url)
+        track.save()
+        return CreateTrack(track=track)
+
+class Mutation(graphene.ObjectType):
+      created_track = CreateTrack.Field()
