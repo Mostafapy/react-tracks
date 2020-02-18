c627ab1770a825ff9fe4234eb257fa1aaedc1075
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-02-18 08:36:49 -0800

    add create like mutation to track schema

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Track
+from .models import Track, Like
+from users.schema import UserType
 
 class TrackType(DjangoObjectType):
     """Class that contains all the Tracks called by graphene"""
@@ -64,9 +65,6 @@ class DeleteTrack(graphene.Mutation):
 
     class Arguments:
         track_id = graphene.Int(required=True)
-        title = graphene.String()
-        description = graphene.String()
-        url = graphene.String() 
 
     def mutate(self, info, track_id):
         user = info.context.user
@@ -79,7 +77,31 @@ class DeleteTrack(graphene.Mutation):
         track.delete()
         return DeleteTrack(track_id=track_id)
 
+class CreateLike(graphene.Mutation):
+    user = graphene.Field(UserType)
+    track = graphene.Field(TrackType)
+     class Arguments:
+            track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        user = info.context.user
+
+         if user.is_anonymous:
+             raise Exception('Log in first to like the track.')
+
+        track = Track.objects.get(id=track_id)
+
+        if not track:
+            raise Exception('there no traack for the provided track')
+        
+        Like.objects.create(
+            user=user,
+            track=track
+        )
+
+        return CreateLike(user=user, track=track)
 class Mutation(graphene.ObjectType):
       created_track = CreateTrack.Field()
       update_track = UpdateTrack.Field()
       delete_track = DeleteTrack.Field()
+      create_like = CreateLike.Field()
